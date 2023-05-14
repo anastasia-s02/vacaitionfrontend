86d8ec0a9d37d6541452c03982f95779745b4880
Nathanuel Dixon <nathan0322@hotmail.com>
2023-05-14 03:11:02 -0400

    hah

--- src/screens/SignUpScreen.jsx
+++ src/screens/SignUpScreen.jsx
@@ -8,7 +8,11 @@ export default function SignupScreen() {
   const location = useLocation();
   // const { passedobj } = location.state;
   // if an object was passed in, it will be stored in passedobj, otherwise passedobj will be undefined
-  const { passedobj } = location.state;
+  let passedobj = {};
+  // const { passedobj } = location.state;
+  if (location.state) {
+    passedobj = location.state.passedobj;
+  }
   const navigate = useNavigate();
   const [email, setEmail] = useState();
   const [password, setPassword] = useState();

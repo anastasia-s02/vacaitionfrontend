a980bd6d728ad02ac45dc7babd0caf6fffa7daa7
Abigail Zhou <alz4471@nyu.edu>
2023-05-14 12:50:34 -0400

    button

--- src/screens/results/Results.jsx
+++ src/screens/results/Results.jsx
@@ -55,6 +55,11 @@ export default function Results(){
                     </div>
                 ))}
             </div>
+            <button id="find-people" onClick={async (e) => {
+                //
+            }}>
+                Looking for a travel buddy?
+            </button>
         </div>
     )
   }
